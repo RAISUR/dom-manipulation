17d1b94940e188946088e2eb0d0a901e6a3b3ff4
raisur <raisur.rahman234@gmail.com>
2018-10-25 16:22:35 +0000

    classwor_5.

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
  
-var heading=document.getElementById('heading');
+/* Task 1 */
+var heading = document.getElementById('heading');       //Always returns an array;
 console.log(heading.innerText);
-/*heading.innerText="Changed it";*/
-/*heading.innerHTML=<h3>"Changed it"</h3>;*/
+
 /* Task 2 using plain JavaScript */
-var textBtn=document.getElementById('addText2');
-textBtn.addEventListener('click',function(event){
-   var task2=document.getElementById('task2a');
-   task2.innerText="Hello World";
-   var paragraph = document.createElement('p');
-   paragraph.innerText="Hello World";
-   task2.appendChild('paragraph');
+//Task 2a:
+var txtBtn = document.getElementById('addText2btn');
+txtBtn.addEventListener('click', function(event){
+    var task2 = document.getElementById('task2a');
+    var p = document.createElement('p');
+    p.innerText = "Hello World";
+    task2.appendChild(p);
 });
 
+//Task 2b:
 function changeBGcolor(event){
-    var body=document.getElementsByTagName('body')[0];
-    if(event.target.innerText=="Red"){
-        body.style.backgroundColor='red';
-    }
-    if(event.target.innerText=="Green"){
-        body.style.backgroundColor='green';
-    }
-}
-//task 2c
-function redirecttoGoogle(event){
-    window.location.assign("http://www.google.com")
+    var bg = document.getElementsByTagName('body')[0];
+    bg.style.backgroundColor = event.target.innerText.toLowerCase();
 }
 
+//Task 2c:
+function goToGoogle(event){
+    window.location.assign("https://www.google.com");
+}
 /* Task 4 using jQuery */
+//Task 4a
+$('#addText4').click(function(event){
+    var paragraph = $('<p>').text('Hello World');
+    $('#task4a').append(paragraph);
+});
+
+//Task 4b
+function changeBG(event){
+    var color = event.target.innerText.toLowerCase();
+    $('body').css('background-color', color);
+}
+
+//Task 4c
+$('#sum').click(function(event){
+    var s = Number($("#num1").val()) + Number($("#num2").val());
+    $('#result').text(s);
+});
